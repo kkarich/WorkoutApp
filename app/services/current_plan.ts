fe7e43f54b813379d6f34065e3b7ff79435f7cf2
Keenan Karich <keenanmkarich@gmail.com>
2016-02-29 17:36:38 -0600

    added next workout method to currentPlanService

--- app/services/current_plan.ts
+++ app/services/current_plan.ts
@@ -1,12 +1,12 @@
 import {Injectable} from "angular2/core";
 
-import {IWorkout} from "../interfaces/workout" 
+import {IWorkout} from "../interfaces/workout"
 @Injectable()
 export class CurrentPlanService {
-    id:Number;
-    name:String;
-    description:String;
-    workouts:Array<IWorkout>;
+    id: Number;
+    name: String;
+    description: String;
+    workouts: Array<IWorkout>;
     constructor() {
         this.id = 6;
         this.name = "3 Day Split";
@@ -20,56 +20,56 @@ export class CurrentPlanService {
                         "id": 8,
                         "name": "Bench Press",
                         "exercise_id": 2,
-                        "sets": 3,
-                        "reps": 10,
+                        "setGoal": 3,
+                        "repGoal": 10,
                         "musclegroup": "chest"
                     },
                     {
                         "id": 9,
                         "name": "Flyes",
                         "exercise_id": 7,
-                        "sets": 3,
-                        "reps": 10,
+                        "setGoal": 3,
+                        "repGoal": 10,
                         "musclegroup": "chest"
                     },
                     {
                         "id": 10,
                         "name": "Incline Bench Press",
                         "exercise_id": 8,
-                        "sets": 3,
-                        "reps": 10,
+                        "setGoal": 3,
+                        "repGoal": 10,
                         "musclegroup": "chest"
                     },
                     {
                         "id": 11,
                         "name": "Incline Flyes",
                         "exercise_id": 9,
-                        "sets": 3,
-                        "reps": 10,
+                        "setGoal": 3,
+                        "repGoal": 10,
                         "musclegroup": "chest"
                     },
                     {
                         "id": 12,
                         "name": "Shoulder Press",
                         "exercise_id": 3,
-                        "sets": 3,
-                        "reps": 10,
+                        "setGoal": 3,
+                        "repGoal": 10,
                         "musclegroup": "Shoulder"
                     },
                     {
                         "id": 13,
                         "name": "Front Raises",
                         "exercise_id": 10,
-                        "sets": 3,
-                        "reps": 10,
+                        "setGoal": 3,
+                        "repGoal": 10,
                         "musclegroup": "Shoulder"
                     },
                     {
                         "id": 14,
                         "name": "Lateral Raises",
                         "exercise_id": 11,
-                        "sets": 3,
-                        "reps": 10,
+                        "setGoal": 3,
+                        "repGoal": 10,
                         "musclegroup": "Shoulder"
                     }
                 ]
@@ -77,8 +77,21 @@ export class CurrentPlanService {
         ];
     }
     // get next workout out if index is passed in, otherwas get first workout
-    getNextWorkout(index){
-        var nextWorkout:IWorkout;
+    getNextWorkout(index?: number) {
+        // if this.workouts does not exist, return empty object
+        if (this.workouts.length < 1) {
+            return {};
+        }
+        
+        // init next workout
+        var nextWorkout: IWorkout;
+        // if index exists and it is not the last element in array, return next workout.
+        // Otherwise, return first workout
+        if (index && index + 1 != this.workouts.length) {
+            nextWorkout = this.workouts[index + 1];
+        } else {
+            nextWorkout = this.workouts[0];
+        }
         return nextWorkout;
     }
 
