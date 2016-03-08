fe1cd5c7b6625a2565da6327bace08110d5f895d
Keenan Karich <keenanmkarich@gmail.com>
2016-03-08 15:17:08 -0600

    added instance interfaces

--- app/interfaces/exercise.ts
+++ app/interfaces/exercise.ts
@@ -1,9 +1,16 @@
 export interface IExercise {
+    exercise_id:String;
+    name:string;
+    rep_goal:Number;
+    set_goal:Number;
+}
+
+export interface IExerciseInstance {
     id?:Number;
     exercise_id:String;
     name:string;
     rep_goal:Number;
     set_goal:Number;
-    weight?:Number;
-    reps?:Array<Number>;
+    weight:Number;
+    reps:Array<Number>;
 }
--- app/interfaces/workout.ts
+++ app/interfaces/workout.ts
@@ -1,9 +1,17 @@
-import {IExercise} from "./exercise"
+import {IExercise,IExerciseInstance} from "./exercise"
 
 export interface IWorkout {
     id?:Number;
     plan_id:Number;
     name: String;
     exercises:Array<IExercise>;
+    
+}
+
+export interface IWorkoutInstance {
+    id?:Number;
+    plan_id:Number;
+    name: String;
+    exercises:Array<IExerciseInstance>;
     
 }
