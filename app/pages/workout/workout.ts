e4e1ab3ca1a80325488bacf13c741b2cd5d0478c
Keenan Karich <keenanmkarich@gmail.com>
2016-03-16 17:38:04 -0500

    added check for completed exercise debounce function

--- app/pages/workout/workout.ts
+++ app/pages/workout/workout.ts
@@ -3,49 +3,79 @@ import {Page, Alert, NavController} from 'ionic-framework/ionic';
 import {CurrentWorkoutService} from '../../services/current_workout';
 import {CurrentPlanService} from '../../services/current_plan';
 import {LogService} from '../../services/log';
+import {debounce} from '../../services/utility';
 
 
 @Page({
     templateUrl: 'build/pages/workout/workout.html'
 })
 export class WorkoutPage {
-    
     // init properties
     workout;
     plan: CurrentPlanService;
     log: LogService
     nav: NavController
+    exerciseIsCompleted;
 
     constructor(nav: NavController, currentPlan: CurrentPlanService, currentWorkout: CurrentWorkoutService, log: LogService) {
         this.workout = currentWorkout;
         this.plan = currentPlan;
         this.log = log;
         this.nav = nav;
+
+        this.exerciseIsCompleted = debounce((exercise) => {
+            // default to true, for easier 
+            var isCompleted = true;
+
+            // check each rep to make sure it is not null 
+            for (var i in exercise.reps) {
+                var rep = exercise.reps[i];
+                // if rep is null return false
+                if (!rep && rep !== 0) {
+                    return;
+                }
+            }
+            // if the exercise is completed, make sure to handle it
+            this.handleCompletedExercise(exercise);
+        }, 1000, false);
+
     }
-    
+
     // save the workout
     save() {
         this.workout.completed = true;
-        
+
         // pass in this workout to log save method
         this.log.logWorkout(this.workout).then((resp) => {
             this.workout.init();
             this.nav.pop();
         });
     }
-    
-    // login for when someone clicks on a rep
+
+    // logic for when someone clicks on a rep
     handleRepClick(exerciseIndex, repIndex) {
         var exercise = this.workout.exercises[exerciseIndex];
-        if (!exercise.reps[repIndex]) {
+
+        // 3 options. Either rep is null needs to be set to max, 0 needs to be set to null, or it can be decremented
+        if (exercise.reps[repIndex] !== 0 && !exercise.reps[repIndex]) {
             exercise.reps[repIndex] = exercise.rep_goal;
         } else if (exercise.reps[repIndex] === 0) {
             exercise.reps[repIndex] = null;
         } else {
             exercise.reps[repIndex]--;
         };
+
+        // check to see if exercise is completed
+        this.exerciseIsCompleted(exercise);
     }
 
+    // handles completed exercise. ie: markes as complete, calculates suggested weight, ...
+    handleCompletedExercise(exercise) {
+        console.log("COMPLETED")
+        exercise.completed = true;
+        exercise.suggestedWeight = exercise.weight + 5;
+    }
+    // prompt to adjust weight for clicked exercise
     adjustWeightPrompt(exercise) {
         let prompt = Alert.create({
             title: 'Adjust Weight',
@@ -75,6 +105,8 @@ export class WorkoutPage {
         });
         this.nav.present(prompt);
     }
+
+    // prompt to change workout to different workout in plan
     changeWorkoutPrompt() {
         // Init prompt values
         let prompt = Alert.create({
@@ -107,11 +139,11 @@ export class WorkoutPage {
             });
 
         }
-        
+
         // present alert to screen
         this.nav.present(prompt);
     }
-    
+
     // get value from passed in select object
     changeWorkout(value) {
         if (value && value != this.workout.index) {
---
+++ app/services/utility.ts
@@ -0,0 +1,16 @@
+// grabbed from: https://davidwalsh.name/javascript-debounce-function
+// couldnt find an easy way to use angular 2 debounce
+export function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
